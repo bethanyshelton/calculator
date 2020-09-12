317d0cb000778f8b8891b723b21c53562aeeed63
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2020-09-12 19:09:01 -0400

    Add basic calculator functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,26 @@
-document.getElementById("one").onclick = console.log("1");
+// basic calculations
+
+// add
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+//multiply
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+//subtract
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+//divide
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+//calculate
+function calculator(num1, num2, operator) {
+	return operator(num1, num2);
+}
